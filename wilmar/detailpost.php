a04e3b93100fc6e1a9f0aff7e04ce42fbc01dddb
unknown <CLIENT@22REG.PC04>
2018-02-12 05:49:34 +0800

    page

--- wilmar/detailpost.php
+++ wilmar/detailpost.php
@@ -9,7 +9,10 @@
                 </div>
             </div>
             <div id="breadcrumbWrapper">
-                <div id="breadcrumb"><a href="/">Home</a> <img width="10" height="10" alt="arrow" src="https://www.gwu.edu/sites/all/themes/gwu_marketing/images/breadcrumb_arrow.png"></img> <span>Admissions & Aid</span></div>
+                <div id="breadcrumb">
+				<a href="<?=BASE_URL;?>">Home</a> 
+				<img width="10" height="10" alt="arrow" src="https://www.gwu.edu/sites/all/themes/gwu_marketing/images/breadcrumb_arrow.png"></img> 
+				<span><?=$this->e($front_post_title);?></span></div>
             </div>
             <a id="main-content"></a>
             <div id="block-system-main" class="block block-system first last odd count-1 count-1">
@@ -22,7 +25,7 @@
 
 
                                 <h1 class="pane-title">
-                                    Admissions &amp; Aid </h1>
+                                 <?=$post['title'];?></h1>
 
                             </header>
 
@@ -33,33 +36,19 @@
                                      
                                         <div class="container">
                                             <div class="row">
-                                                <div class="col-md-12">
-                                                    <article>
-                                                        <header class="lai-one">
-                                                            <h2></h2>
-                                                        </header>
-                                                        <div class="lai-no">
-                                                            <div class="lai-img-c lai-two">
-                                                                <figure>
-                                                                    <p class="i-wrap"><img alt="student holding colonials sign" class="img-responsive" src="https://www.gwu.edu/sites/www.gwu.edu/files/image/ColonialSign_AdmissionsandAidmainimage_Move_IN_UP_WLA_2009-7850_CUTOUT.jpg"
-                                                                        /></p>
-                                                                    <figcaption class="t-img-caption">
-                                                                        <p>&nbsp;</p>
-                                                                    </figcaption>
-                                                                </figure>
-                                                            </div>
-                                                            <div class="lai-copy lai-three">
-                                                                <p>At the George Washington University (GW), #OnlyatGW is not just a hashtag&mdash;it is our way of life. Living and learning in the heart of D.C. provides our students opportunities and experiences
-                                                                    unlike anywhere else. Inside the classroom, you&rsquo;ll sit next to peers from all over the world and learn from faculty who are not only experts in their fields, but who also might
-                                                                    be world-famous artists, policy-makers or dignitaries.&nbsp;</p>
-                                                                <p>At GW, learning doesn&rsquo;t stop when class is over. It&#39;s the reason you&rsquo;re here and will be a part of everything you&rsquo;ll do. You can use our resources to forge your own
-                                                                    path forward. You might find a dream internship at a non-profit or the White House, volunteer to better our city, run into government leaders on campus, or even study on the steps of
-                                                                    monuments that tourists flock to. You&rsquo;ll not only live and learn at the center of our nation&rsquo;s capital, but you&rsquo;ll be a central part of it no matter your path.&nbsp;</p>
-                                                               
-                                                            </div>
-                                                        </div>
-                                                    </article>
-                                                </div>
+                                                <div class="col-sm-12 col-xs-12 pos-one">
+    <article>
+        <p class="col-img">
+		<img alt="" class="img-responsive" src="<?=BASE_URL;?>/<?=DIR_CON;?>/uploads/<?=$post['picture'];?>"
+                style="height:720px; width:1080px"></p>
+        <header class="col-h">
+            <h3 class="promo-title"><b><?=$post['picture_description'];?></b></h3>
+        </header>
+        <section class="col-copy">
+            <p><?=htmlspecialchars_decode(html_entity_decode($post['content']));?></p>
+            </section>
+    </article>
+</div>
                                             </div>
                                         </div>
                                       
